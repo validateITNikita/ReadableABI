2f48c491fab9e690e8834e07e4f29c1198352c70
NIKITA VELIX <nikita.kuznetsov2017@yandex.ru>
2022-12-18 06:41:13 +0300

    code formated

--- src/components/TextArea.vue
+++ src/components/TextArea.vue
@@ -1,31 +1,32 @@
 <script setup lang="ts">
-import { computed, onMounted, ref } from 'vue';
+import { ref } from 'vue';
 import {ethers} from 'ethers';
 
-const notReadableABI = ref<string>('');
-
-const notread = ref(null);
-const readable = ref(null);
+const notread = ref<HTMLTextAreaElement>();
+const readable = ref<HTMLTextAreaElement>();
 
 const convert = () => {
-  // @ts-ignore
-  const rea = new ethers.utils.Interface((notread.value as HTMLTextAreaElement).value);
-  // @ts-ignore
-  (readable.value as HTMLTextAreaElement).value = JSON.stringify(rea.format(ethers.utils.FormatTypes.full), null, 2) ;
+  if(notread.value && readable.value){
+    let temp = new ethers.utils.Interface(notread.value.value);
+    let formated = temp.format(ethers.utils.FormatTypes.full);
+    
+    readable.value.value = JSON.stringify(formated, null, 2) ;
+  }
 }
+
+const areaStyle = "block p-2.5 w-full h-full text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-left";
 </script>
 
 <template>
-  <div class="flex items-center justify-center space-x-5 p-10 w-full h-[500px]">
+  <div class="flex flex-col md:flex-row items-center justify-center space-y-5 md:space-y-0 md:space-x-5 p-10 w-full h-[500px]">
     <textarea 
-      class="block p-2.5 w-full h-full text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-left"
+      :class="areaStyle"
       placeholder="Paste your ABI here!"
-      v-model="notReadableABI"
       ref="notread"
       />
       <button @click="convert">CLICK</button>
     <textarea 
-      class="block p-2.5 w-full h-full text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-left"
+      :class="areaStyle"
       placeholder="Copy your ABI from here!"
       ref="readable"
       />
